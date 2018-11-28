d0dccdb060507f174101679fbc552f1ce3f52682
伊藤 智之 <88429@ad.exa-corp.co.jp>
2018-11-28 10:13:25 +0900

    l

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 "# concourse_test" 
+cf login -a api.run.pivotal.io -u tomoyuki-ito@exa-corp.co.jp -p P@ssw0rd
+
+cf push kantanPro -p target/kantanPro-0.0.1-SNAPSHOT.jar -b https://github.com/cloudfoundry/java-buildpack.git
--- kantanPro/src/main/java/com/example/demo/controller/ConChanController.java
+++ kantanPro/src/main/java/com/example/demo/controller/ConChanController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ConChanController {
 
-	@RequestMapping(value = "/con")
+	@RequestMapping(value = "/")
 	public String index() {
 		return "conchan";
 	}
